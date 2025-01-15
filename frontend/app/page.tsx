dcfbfe8861f97e0f477a5e9562947b63a33eb08f
David Yang <dyang195@gmail.com>
2025-01-15 14:57:16 -0800

    frontend nit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useCallback } from 'react'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 import Header from '../components/Header'
 import SearchBox from '../components/SearchBox'
